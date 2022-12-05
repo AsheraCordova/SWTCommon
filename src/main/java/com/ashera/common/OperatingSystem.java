6d1cd55726fb0d0822a155b5fb009b798321ce0c
Ashera Cordova <ashera.cordova@gmail.com>
2022-12-05 10:44:47 +0530

    Added getOs in OperatingSystem class

--- src/main/java/com/ashera/common/OperatingSystem.java
+++ src/main/java/com/ashera/common/OperatingSystem.java
@@ -9,4 +9,20 @@ public class OperatingSystem {
     public static boolean isOSX() {
         return (OS.indexOf("mac os x") >= 0);
     }
+    
+    public static boolean isLinux() {
+        return (OS.indexOf("linux") >= 0);
+    }
+    
+    public static String getOs() {
+    	if (isWindows()) {
+    		return "windows";
+    	}
+    	
+    	if (isOSX()) {
+    		return "mac";
+    	}
+    	
+    	return "linux";
+    }
 }
